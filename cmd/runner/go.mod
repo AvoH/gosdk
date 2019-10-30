e6f11d9236c870282de62bd95c2b9c16d6896b1a
Avetik Hovhannisyan <avetik@vmware.com>
2019-10-29 21:37:59 -0700

    Adding translator

--- cmd/runner/go.mod
+++ cmd/runner/go.mod
@@ -2,4 +2,7 @@ module github.com/AvoH/gosdk/cmd/runner
 
 go 1.13
 
-require github.com/AvoH/gosdk/converter v0.2.1
+require (
+	github.com/AvoH/gosdk/converter v0.2.1
+	github.com/AvoH/gosdk/translator v0.2.0
+)
--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/AvoH/gosdk/converter"
+	"github.com/AvoH/gosdk/translator"
 )
 func main() {
 	fmt.Println(converter.Convert())
+	fmt.Println(translator.Translate())
 }
